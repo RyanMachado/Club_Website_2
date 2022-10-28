dd0fe38b1d83fd10c150f68bfc8584de5764f826
RyanMachado <machado1.ryan@gmail.com>
2022-10-28 17:47:49 -0400

    started 'Become a member' section

---
+++ test/auth.spec.js
@@ -0,0 +1,12 @@
+const expect = chai.expect;
+
+describe('template', function() {
+    describe('clearCompletedButton', function() {
+        it('should return "Clear completed" when more than 0 items are completed', function() {
+            var template = new app.Template();
+            var result = template.clearCompletedButton(2);
+            expect(result).to.equal('Clear completed');
+        });
+    });
+});
+
